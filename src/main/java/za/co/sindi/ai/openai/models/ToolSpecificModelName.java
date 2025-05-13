387e1daf1be431975b653b30781f3a53ac71550c
Buhake Sindi <buhake_sindi@hotmail.com>
2025-05-13 02:26:43 +0200

    Refined all OpenAI model names to conform to the latest model listing.

--- src/main/java/za/co/sindi/ai/openai/models/ToolSpecificModelName.java
+++ src/main/java/za/co/sindi/ai/openai/models/ToolSpecificModelName.java
@@ -9,9 +9,12 @@ package za.co.sindi.ai.openai.models;
  * @see <a href="https://platform.openai.com/docs/models#transcription">Transcription Model</a>
  */
 public enum ToolSpecificModelName {
-	GPT_4O_TRANSCRIBE("gpt-4o-transcribe")
-	,GPT_4O_MINI_TRANSCRIBE("gpt-4o-mini-transcribe")
-	,WHISPER_1("whisper-1")
+	GPT_4O_SEARCH_PREVIEW("gpt-4o-search-preview")
+	,GPT_4O_SEARCH_PREVIEW_2025_03_11("gpt-4o-search-preview-2025-003-11")
+	,GPT_4O_MINI_SEARCH_PREVIEW("gpt-4o-mini-search-preview")
+	,GPT_4O_MINI_SEARCH_PREVIEW_2025_03_11("gpt-4o-mini-search-preview-2025-003-11")
+	,COMPUTER_USE_PREVIEW("computer-use-preview")
+	,COMPUTER_USE_PREVIEW_2025_03_11("computer-use-preview-2025-03-11")
 	;
 	private final String model;
 
